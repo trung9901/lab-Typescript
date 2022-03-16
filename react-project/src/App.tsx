80047fad308919b639913d259a3d7bd70e50d147
Trung <trungnhph13487@fpt.edu.vn>
2022-03-17 06:52:21 +0700

    a

---
+++ react-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      1223
+    </div>
+  )
+}
+
+export default App
