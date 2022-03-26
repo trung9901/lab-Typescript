7f1132c603a293940f41d189bcb3d9fa7d361858
Trung <trungnhph13487@fpt.edu.vn>
2022-03-26 12:14:13 +0700

    a

--- react-project/src/api/product.tsx
+++ react-project/src/api/product.tsx
@@ -4,6 +4,18 @@ export const list = () => {
     const url = "/products";
     return instance.get(url);
 }
+export const get = (id: number) => {
+    const url = `/product/${id}`
+    return instance.get(url);
+}
+export const add = (product: {}) => {
+    const url = "/products"
+    return instance.post(url, product);
+}
+export const update = (product: {}) => {
+    const url = `/product/${product.id}`;
+    return instance.put(url, product);
+}
 export const remove = (id: number) => {
     const url = `/product/${id}`;
     return instance.delete(url);
